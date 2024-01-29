bca1ddd5fc081adf4da65b92d8ac667ff2214dcc
ぶち <6uclz1@users.noreply.github.com>
2024-01-29 14:06:18 +0900

    Update books data.

--- convert.js
+++ convert.js
@@ -1,10 +1,23 @@
-// import fs from 'fs'
-// import { parse } from 'csv-parse/sync'
-
 const fs = require('fs')
 const { parse } = require('csv-parse/sync')
 
-const data = fs.readFileSync('../public/books.csv')
+const data = fs.readFileSync('./public/books.csv')
+
 const d = parse(data, {columns:true})
 
-console.log(d)
+const formattedData = d.map((row) => {
+    return {
+        title: row.title,
+        author: row.author,
+        publisher: row.publisher,
+        isbn: row.isbn,
+        readDate: row.readDate,
+        thumnailImage: row.thumnailImage,
+    }
+})
+
+console.log(formattedData)
+
+const json = JSON.stringify(formattedData, null, 2);
+
+fs.writeFileSync('export.json', json);
