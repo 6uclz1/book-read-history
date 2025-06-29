b7264a7de0e66c16d23c71450b7b1c9592b72af1
ぶち <6uclz1@users.noreply.github.com>
2025-06-29 20:46:55 +0900

    アクセシビリティのLighthouseスコアの警告レベルを変更し、最小スコアを0.8に調整

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,7 +10,7 @@ module.exports = {
     assert: {
       assertions: {
         'categories:performance': ['warn', { minScore: 0.8 }],
-        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.8 }],
         'categories:best-practices': ['warn', { minScore: 0.8 }],
         'categories:seo': ['warn', { minScore: 0.8 }],
         'categories:pwa': 'off',
